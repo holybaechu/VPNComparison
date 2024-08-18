135cf6f80425125c3c446595bd8c6545e2efd548
holybaechu <holybaechu@proton.me>
2024-08-18 17:48:12 +0900

    Implement total locations for PIA

--- services/PIA.ts
+++ services/PIA.ts
@@ -17,18 +17,21 @@ export async function test(): Promise<Result> {
     const continents: Continents = await fetch("https://assets-cms.privateinternetaccess.com/servers/pia/servers_countries_continents_en.json").then(res => res.json())
 
     let totalServers = 0
+    let totalLocations = 0
     let totalCountries = 0
     for (const continentName in continents) {
         const continent = continents[continentName]
         
         for (const country of continent) {
             totalServers += country.servers
+            totalLocations += country.cities.length
         }
         totalCountries += continent.length
     }
 
     return {
         totalServers: totalServers,
+        totalLocations: totalLocations,
         totalCountries: totalCountries
     }
 }
